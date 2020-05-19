0b5c5f2747af4370cbd0b92c567b3a25f95563dc
piyush ravi <mails4ipiyush@gmail.com>
2020-05-19 21:52:52 +0530

    replace 0s and 1s with enum

--- learnCPP/printBoard.cpp
+++ learnCPP/printBoard.cpp
@@ -4,29 +4,41 @@
 #include<sstream>
 #include<fstream>
 
-std::vector<int> ParseLine(std::string line_with_comma) {
+enum class State {kEmpty, kObstacle};
+
+std::vector<State> ParseLine(std::string line_with_comma) {
     std::istringstream stream(line_with_comma);
-    std::vector<int> vector;
+    std::vector<State> vector;
     std::string s;
     while(getline(stream, s, ',')) {
         int n = std::stoi(s);
-        vector.push_back(n);
+        if(n == 0) {
+            vector.push_back(State::kEmpty);
+        }
+        else {
+            vector.push_back(State::kObstacle);
+        }
     }
     return vector;
 }
 
-void PrintBoard(std::vector<std::vector<int>>& board) {
+std::string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return "⛰️   ";
+    default: return "0   "; 
+  }
+}
+
+void PrintBoard(std::vector<std::vector<State>>& board) {
     for(auto i = 0; i != board.size(); i++) {
         for(auto j = 0; j != board[1].size(); j++) {
-            std::cout<< board[i][j] << "\t";
+            std::cout<< CellString(board[i][j]);
         }
         std::cout << std::endl;
     }
-
-    board[0][0] = 4;
 }
 
-std::vector<std::vector<int>> ReadBoardFile(std::string& filename) {
+std::vector<std::vector<State>> ReadBoardFile(std::string& filename) {
     std::ifstream inFile(filename);
 
     if(!inFile) {
@@ -34,9 +46,9 @@ std::vector<std::vector<int>> ReadBoardFile(std::string& filename) {
         exit(1);
     }
     std::string s{};
-    std::vector<std::vector<int>> board;
+    std::vector<std::vector<State>> board;
     while (getline(inFile, s)) {
-        std::vector<int> line = ParseLine(s);
+        std::vector<State> line = ParseLine(s);
         board.push_back(line);
     }
     return board;
@@ -44,17 +56,15 @@ std::vector<std::vector<int>> ReadBoardFile(std::string& filename) {
 
 void TestParseLineMethod() {
     std::string s{"0,1,2,3,"};
-    std::vector<int> result = ParseLine(s);
+    std::vector<State> result = ParseLine(s);
     for(int i = 0; i != result.size(); i++) {
-        std::cout << result[i] << std::endl;
+        std::cout << static_cast<int>(result[i]) << std::endl;
     }
 } 
 
 int main() {
-    //std::vector<std::vector<int>>  board{{0,1,0,0,0,0},{0,1,0,0,0,0}, {0,1,0,0,0,0}, {0,1,0,0,0,0}, {0,0,0,0,1,0}};
-    //PrintBoard(board);
-    std::string file{"1.board"};
-    std::vector<std::vector<int>> result = ReadBoardFile(file);
-    PrintBoard(result);
+    std::string fileName{"1.board"};
+    auto board = ReadBoardFile(fileName);    
+    PrintBoard(board);
     return 0;
 }
